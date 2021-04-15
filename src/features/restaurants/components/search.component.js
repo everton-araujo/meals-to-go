f4d5447f859717153e5df799e855eac67c97a43e
Everton <everton.aaraujo@outlook.com>
2021-04-15 16:16:59 -0300

    Search funcionality

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Searchbar } from "react-native-paper";
 
 import { LocationContext } from "../../../services/location/location.context";
@@ -8,6 +8,10 @@ import { SearchContainer } from "./search.style";
 export const Search = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    search(searchKeyword);
+  }, []);
 
   return (
     <SearchContainer>
